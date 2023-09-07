7049ce231141875f368f9d2a3d91ca58c78645db
Meng Kiat <116622260+mkiats@users.noreply.github.com>
2023-09-08 01:26:57 +0800

    overflow settled

--- tiktok/lib/06_wishlist_page.dart
+++ tiktok/lib/06_wishlist_page.dart
@@ -24,37 +24,41 @@ class _WishlistPageState extends State<WishlistPage> {
           color: Colors.black, // Icon color
         ),
       ),
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            WishlistItem(
-              itemName: 'Keyboard',
-              itemDescription:
-                  'Mechanical, Tactile switches, Ergonomic, Wireless',
-              itemImage:
-                  'https://images.unsplash.com/photo-1595044426077-d36d9236d54a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
-            ),
-            WishlistItem(
-              itemName: 'Shampoo',
-              itemDescription: 'Natural ingredients, Everyday use',
-              itemImage:
-                  'https://images.unsplash.com/photo-1629196869698-2ce173dacc24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
-            ),
-            WishlistItem(
-              itemName: 'Coffee beans',
-              itemDescription: 'Arabica, Roasted',
-              itemImage:
-                  'https://images.unsplash.com/photo-1625021659159-f63f546d74a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
-            ),
-            WishlistItem(
-              itemName: 'Diffuser',
-              itemDescription: 'Essential oil diffuser, Scented oil diffuser',
-              itemImage:
-                  'https://images.unsplash.com/photo-1625480493029-abd01cd6061d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
-            ),
-          ],
+      body: FractionallySizedBox(
+        heightFactor: 1,
+        widthFactor: 1,
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              WishlistItem(
+                itemName: 'Keyboard',
+                itemDescription:
+                    'Mechanical, Tactile switches, Ergonomic, Wireless',
+                itemImage:
+                    'https://images.unsplash.com/photo-1595044426077-d36d9236d54a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+              ),
+              WishlistItem(
+                itemName: 'Shampoo',
+                itemDescription: 'Natural ingredients, Everyday use',
+                itemImage:
+                    'https://images.unsplash.com/photo-1629196869698-2ce173dacc24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+              ),
+              WishlistItem(
+                itemName: 'Coffee beans',
+                itemDescription: 'Arabica, Roasted',
+                itemImage:
+                    'https://images.unsplash.com/photo-1625021659159-f63f546d74a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+              ),
+              WishlistItem(
+                itemName: 'Diffuser',
+                itemDescription: 'Essential oil diffuser, Scented oil diffuser',
+                itemImage:
+                    'https://images.unsplash.com/photo-1625480493029-abd01cd6061d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -66,7 +70,8 @@ class WishlistItem extends StatelessWidget {
   final String itemDescription;
   final String itemImage;
 
-  const WishlistItem({super.key, 
+  const WishlistItem({
+    super.key,
     required this.itemName,
     required this.itemDescription,
     required this.itemImage,
@@ -74,43 +79,47 @@ class WishlistItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 16.0),
-      child: Row(
-        children: [
-          Container(
-            width: 80.0,
-            height: 80.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.0),
-              image: DecorationImage(
-                image: NetworkImage(itemImage),
-                fit: BoxFit.cover,
+    return FractionallySizedBox(
+      widthFactor: 0.9,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 16.0),
+        child: Row(
+          children: [
+            Container(
+              width: 80.0,
+              height: 80.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                image: DecorationImage(
+                  image: NetworkImage(itemImage),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          const SizedBox(width: 16.0),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                itemName,
-                style: const TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
+            const SizedBox(width: 16.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  itemName,
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 4.0),
-              Text(
-                itemDescription,
-                style: const TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.grey,
+                const SizedBox(height: 4.0),
+                Text(
+                  itemDescription,
+                  style: const TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.grey,
+                      overflow: TextOverflow.ellipsis),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
