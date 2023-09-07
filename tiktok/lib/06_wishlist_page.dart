49972e2f22673e3f32d22d8e32029a5eb5e6b26a
jklye <lyejinkai@gmail.com>
2023-09-07 16:18:15 +0800

    clickable wishlist, redirect to wishlist page

---
+++ tiktok/lib/06_wishlist_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WishlistPage extends StatefulWidget {
+  const WishlistPage({super.key});
+
+  @override
+  State<WishlistPage> createState() => _WishlistPageState();
+}
+
+class _WishlistPageState extends State<WishlistPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('wishlist Page'),
+      ),
+    );
+  }
+}
--- tiktok/lib/main.dart
+++ tiktok/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:tiktok/04_inbox_page.dart';
 import 'package:tiktok/05_0_profile_page_self.dart';
 import 'package:tiktok/01_home_page.dart';
 import 'package:tiktok/05_1_profile_page_others.dart';
+import 'package:tiktok/06_wishlist_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,11 +33,12 @@ class RootPage extends StatefulWidget {
 class _RootPageState extends State<RootPage> {
   int currentPage = 0;
   final pages = [
-    const HomePage(),
+    HomePage(),
     const DiscoverPage(),
     const CreateStoryPage(),
     const ProfilePageOthers(),
     const ProfilePageSelf(),
+    const WishlistPage(),
   ];
   @override
   Widget build(BuildContext context) {
@@ -57,164 +59,6 @@ class _RootPageState extends State<RootPage> {
         },
         selectedIndex: currentPage,
       ),
-    );
-  }
-}
-
-class CustomBottomNavigationBar extends StatelessWidget {
-  final int currentIndex;
-  final ValueChanged<int> onTap;
-
-  CustomBottomNavigationBar({required this.currentIndex, required this.onTap});
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-      ],
-      currentIndex: currentIndex,
-      onTap: onTap,
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        // Video or image content
-        Image.network(
-          'https://images.unsplash.com/photo-1529973625058-a665431328fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
-          fit: BoxFit.cover,
-          width: double.infinity,
-          height: double.infinity,
-        ),
-        // Icons at the right side
-        Align(
-          alignment: Alignment.bottomRight,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.person_add,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    // person add action
-                  },
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.favorite_border,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    // like action
-                  },
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.comment,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    // comment action
-                  },
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.share,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    // share action
-                  },
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.person,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    // view profile action
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
-        // Text at the bottom left corner
-        Align(
-          alignment: Alignment.bottomLeft,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Align(
-              alignment: Alignment.bottomLeft, // Align text to the bottom left
-              child: Column(
-                mainAxisAlignment:
-                    MainAxisAlignment.end, // Align text to the bottom
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 4.0),
-                    child: Text(
-                      '@mkiats',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 4.0),
-                    child: Text(
-                      'its the time of the year! 🎅🎄 #christmas #santa #gifts #snow',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14.0,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 4.0),
-                    child: Text(
-                      '🎵 All I Want For Christmas Is You',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14.0,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    '🎁 Wishlist',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ProfilePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // profile page implementation
-    return Center(
-      child: Text('Profile Screen'),
     );
   }
 }
