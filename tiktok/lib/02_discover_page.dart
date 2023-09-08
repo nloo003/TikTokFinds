be8bce3984d0a85cec59a172019f6d233798d0c1
appleworm34 <xavieryeo2@gmail.com>
2023-09-08 23:44:44 +0800

    Add API functionality

--- tiktok/lib/02_discover_page.dart
+++ tiktok/lib/02_discover_page.dart
@@ -1,7 +1,11 @@
 // ignore_for_file: file_names
 import 'package:flutter/material.dart';
+import '06_wishlist_page.dart';
 import 'dart:math';
 import 'model/wishlist_model.dart';
+import 'model/item_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class DiscoverPage extends StatefulWidget {
   const DiscoverPage({super.key});
@@ -13,29 +17,69 @@ class DiscoverPage extends StatefulWidget {
 class _DiscoverPageState extends State<DiscoverPage> {
 
   List<String> hintTextList = ["iPhone 11 pro case", "Travel adapter", ];
-  static List<WishlistModel> wishlistList = [
-    WishlistModel("atitle", "adesc", "acreatorId", "acreatorName", "https://images.unsplash.com/photo-1529973625058-a665431328fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"),
-    WishlistModel("btitle", "bdesc", "bcreatorId", "bcreatorName", "https://images.unsplash.com/photo-1529973625058-a665431328fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"),
-    WishlistModel("ctitle", "cdesc", "ccreatorId", "ccreatorName", "https://images.unsplash.com/photo-1529973625058-a665431328fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"),
-  ];
-  List<WishlistModel> displayList = List.from(wishlistList);
+  List<WishlistModel> wishlistList = [];
+  List<WishlistModel> displayList = [];
   final _random = Random();
   String hintWord = "";
+  String searchWord = "";
   bool hintWordSet = false;
+  TextEditingController textController = TextEditingController();
+  String imageUrl = 'https://images.unsplash.com/photo-1529973625058-a665431328fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80';
 
   void updateList(String value) {
     setState(() {
       displayList = wishlistList.where((element) => 
         element.name!.toLowerCase().contains(value.toLowerCase())).toList();
     });
+    searchWord = value;
+  }
+
+  Future<List<WishlistModel>> getWishList() async {
+    try {
+      var url = Uri.parse('http://10.0.2.2:4000/api/wishList/all');
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final List<dynamic> responseData = jsonDecode(response.body);
+
+        final List<WishlistModel> data = responseData
+          .map((json) => WishlistModel.fromJson(json))
+          .toList();
+        
+        return data;
+      }
+      else {
+        debugPrint('Request failed with status: ${response.statusCode}');
+        return [];
+      }
+    } catch (e) {
+      debugPrint('Error: $e');
+      return []; // Return an empty list in case of an exception
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Call the fetchData function when the widget is initialized
+    getWishList().then((items) {
+      setState(() {
+        wishlistList = items;
+        displayList = List.from(wishlistList);
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!hintWordSet){
+    if (!hintWordSet) {
       hintWord = hintTextList[_random.nextInt(hintTextList.length)];
       hintWordSet = true;
     }
+
+    // if(wishlistList.isNotEmpty) {
+    //   displayList = List.from(wishlistList);
+    // }
 
     return Scaffold(
       body: Column(
@@ -58,12 +102,23 @@ class _DiscoverPageState extends State<DiscoverPage> {
                     ),
                     child: Center(
                       child: TextField(
+                        controller: textController,
                         onChanged: (value) => updateList(value),
                         decoration: InputDecoration(
                           prefixIcon: const Icon(
                             Icons.search,
                             color: Colors.grey,
                           ),
+                          suffixIcon: searchWord.isNotEmpty ? IconButton(
+                            icon: const Icon(
+                              Icons.clear,
+                              color: Colors.grey,
+                            ),
+                            onPressed: () {
+                              textController.clear();
+                              updateList('');
+                            },
+                          ) : null,
                           border: OutlineInputBorder(
                             borderSide: const BorderSide(color: Colors.grey, width: 2.0),
                             borderRadius: BorderRadius.circular(8.0),
@@ -99,38 +154,61 @@ class _DiscoverPageState extends State<DiscoverPage> {
               ],
             ),
           ),
-          const SizedBox(height: 20.0),
+          const SizedBox(height: 5.0),
           Expanded(
             child: ListView.builder(
               padding: const EdgeInsets.only(left: 10.0),
               itemCount: displayList.length,
-              itemBuilder: (context, index) => ListTile(
-                contentPadding: const EdgeInsets.all(8.0),
-                title: Text(
-                  displayList[index].name!,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 16.0, 
+              itemBuilder: (context, index) => GestureDetector(
+                onTap: () {
+                  // Navigator.of(context).push(
+                  //   MaterialPageRoute(
+                  //     builder: (context) => const WishlistPage(),
+                  //   ),
+                  // );
+                  debugPrint("clicked");
+                },
+                child: ListTile(
+                  contentPadding: const EdgeInsets.all(8.0),
+                  title: Text(
+                    displayList[index].name!,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17.0, 
+                    ),
                   ),
-                ),
-                subtitle: Text(
-                  displayList[index].description!,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
+                  subtitle: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 10,
+                        backgroundImage: NetworkImage(
+                          // displayList[index].wishListImage!,
+                          imageUrl
+                        ),
+                      ),
+                      const SizedBox(width: 5.0,),
+                      Text(
+                        displayList[index].creatorName!,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                trailing: Text(
-                  displayList[index].creatorName!
-                ),
-                leading: SizedBox(
-                  width: 60,
-                  height: 60,
-                  child: Image.network(
-                    displayList[index].imageUrl!,
-                    fit: BoxFit.cover,
+                  trailing: Text(
+                    'saveCount'
                   ),
-                )
-              ),
+                  leading: SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: Image.network(
+                      imageUrl,
+                      // displayList[index].wishListImage!,
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                ),
+              )  
             ),
           ),
         ],
--- tiktok/lib/model/wishlist_model.dart
+++ tiktok/lib/model/wishlist_model.dart
@@ -1,9 +1,31 @@
+import 'item_model.dart';
+
 class WishlistModel {
   String? name;
   String? description;
+  List<ItemModel>? items;
   String? creatorId;
   String? creatorName;
-  String? imageUrl;
+  String? wishListImage;
 
-  WishlistModel(this.name, this.description, this.creatorId, this.creatorName, this.imageUrl);
+  WishlistModel(this.name, this.description, this.items, this.creatorId, this.creatorName, this.wishListImage);
+  // WishlistModel(this.name, this.description, this.items, this.creatorId, this.creatorName);
+
+  factory WishlistModel.fromJson(Map<String, dynamic> json) {
+    final List<dynamic>? jsonItems = json['items'];
+    List<ItemModel>? itemsList;
+
+    if (jsonItems != null) {
+      itemsList = jsonItems.map((item) => ItemModel.fromJson(item)).toList();
+    }
+
+    return WishlistModel(
+      json['name'],
+      json['description'],
+      itemsList,
+      json['creatorId'],
+      json['creatorName'],
+      json['wishListImage'],
+    );
+  }
 }
