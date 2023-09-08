ea74551760627f0bab4d037df497d27388f17122
Meng Kiat <116622260+mkiats@users.noreply.github.com>
2023-09-08 23:27:04 +0800

    followers page done

--- tiktok/lib/07_friends_page.dart
+++ tiktok/lib/07_friends_page.dart
@@ -3,6 +3,13 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok/widgets/back_icon.dart';
 
+class User {
+  final String username;
+  final String handle;
+
+  const User({required this.username, required this.handle});
+}
+
 class FriendPageFactory extends StatefulWidget {
   const FriendPageFactory({super.key});
 
@@ -11,24 +18,101 @@ class FriendPageFactory extends StatefulWidget {
 }
 
 class _FriendPageFactoryState extends State<FriendPageFactory> {
+  List<User> users = [
+    const User(username: "Meng Kiat", handle: "mkiats"),
+    const User(username: "Nicky", handle: "NLOO"),
+    const User(username: "Tharun", handle: "DARUN")
+  ];
+
+  String username = "mkiats";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         leading: const BackIcon(),
+        backgroundColor: Colors.black,
+        title: Text(username),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+            child: GestureDetector(
+              onTap: () {},
+              child: const Icon(Icons.add_reaction_outlined),
+            ),
+          ),
+        ],
       ),
-    );
-  }
-}
-
-class SinglePersonTile extends StatelessWidget {
-  const SinglePersonTile({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const FractionallySizedBox(
-      child: Text("XXX TILE"),
+      body: Container(
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                      child: const Text(
+                        "Following 58",
+                        style: TextStyle(color: Colors.white70, fontSize: 15),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                      child: const Text(
+                        "Follwers 30",
+                        style: TextStyle(color: Colors.white70, fontSize: 15),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                      child: const Text(
+                        "Friends 50",
+                        style: TextStyle(color: Colors.white70, fontSize: 15),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: SearchBar(
+                onSubmitted: (value) {},
+                hintText: "Search Users",
+                backgroundColor: MaterialStateProperty.all(Colors.white70),
+                padding: const MaterialStatePropertyAll(
+                    EdgeInsets.fromLTRB(5, 5, 5, 5)),
+                leading: const Icon(
+                  Icons.search_outlined,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: users.length,
+                  itemBuilder: (context, index) {
+                    final user = users[index];
+                    return Card(
+                      child: ListTile(
+                        title: Text(user.username),
+                        subtitle: Text(user.handle),
+                        trailing:
+                            const Icon(Icons.notifications_active_outlined),
+                      ),
+                    );
+                  }),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
